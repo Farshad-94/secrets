9de95607a16187a3f46fc7d46abfc38136ea25bc
Farshad Basereh <49536490+Farshad-94@users.noreply.github.com>
2019-06-23 16:23:45 -0400

    Add SSL Self-Signed Cert and Facebook Signup/in

--- app.js
+++ app.js
@@ -3,14 +3,25 @@
 //requiring implemented packages:
 require('dotenv').config();
 const express = require("express");
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
+
+const options = {
+  key: fs.readFileSync("abels-key.pem"),
+  cert: fs.readFileSync("abels-cert.pem")
+};
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -30,10 +41,13 @@ app.use(passport.session());
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true});
 const userSchema = new mongoose.Schema ({
   email: String,
-  password: String
+  password: String,
+  googleId: String,
+  facebookId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 //to avoid deprecation warning: collection.ensureIndex
@@ -42,13 +56,71 @@ mongoose.set('useCreateIndex', true);
 //passport-local-mongoose configuration:
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
 
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+//Google authentication strategy setup:
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: " http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    //console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+//Facebook authentication strategy setup:
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "https://localhost:8080/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res){
   res.render("home");
 });
+
+app.get("/auth/google",
+  passport.authenticate("google", {scope: ["profile"] })
+);
+
+app.get("/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+    function(req, res) {
+      // Successful authentication, redirect to secrets.
+      res.redirect("/secrets");
+  }
+);
+
+
+app.get("/auth/facebook",
+  passport.authenticate("facebook", {scope: ["public_profile"]}));
+
+app.get("/auth/facebook/secrets",
+  passport.authenticate("facebook", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets.
+    res.redirect("/secrets");
+  });
+
 
 app.get("/login", function(req, res){
   res.render("login");
@@ -106,8 +178,9 @@ app.post("/login", function(req, res){
 
 });
 
-
-
-app.listen(3000, function() {
-    console.log("Server started on port 3000.");
+http.createServer(app).listen(3000, function(){
+  console.log("HTTP listening on port 3000");
+});
+https.createServer(options, app).listen(8080, function(){
+  console.log("HTTPS listening on port 8080");
 });
